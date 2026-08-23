8205541f50bc60448a3a81edad1501307f02b402
eddinerabii <eddine.rabii@gmail.com>
2026-08-23 03:08:56 +0200

    Import webhook error matcher

--- backend/auth-service/src/test/java/com/apimarketplace/auth/web/WebhookControllerTest.java
+++ backend/auth-service/src/test/java/com/apimarketplace/auth/web/WebhookControllerTest.java
@@ -51,6 +51,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.contains;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.*;

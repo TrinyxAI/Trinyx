88933600919db3bff44b46b03624503abe851bb3
eddinerabii <eddine.rabii@gmail.com>
2026-08-23 20:17:04 +0200

    Add authenticated Trinyx Cloud gateway

---
+++ backend/gateway-service/src/main/java/com/apimarketplace/gateway/TrinyxGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.apimarketplace.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrinyxGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrinyxGatewayApplication.class, args);
+    }
+}

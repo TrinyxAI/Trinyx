2c189ab8501c0bc5df804ad167d240a2806e3676
eddinerabii <eddine.rabii@gmail.com>
2026-08-23 02:11:03 +0200

    feat: export paid monolith capability flags

--- frontend/lib/edition/index.ts
+++ frontend/lib/edition/index.ts
@@ -1,4 +1,4 @@
-export { EDITION, IS_CE, IS_CLOUD, IS_MANAGED_CLOUD } from './edition';
+export { EDITION, IS_CE, IS_CLOUD, IS_MANAGED_CLOUD, IS_EMBEDDED_AUTH, IS_BILLING_ENABLED, IS_PAID_MONOLITH, IS_COMMUNITY_EDITION } from './edition';
 export type { Edition } from './edition';
 export { useEdition } from './useEdition';
 export { CLOUD_WEB_BASE_URL, cloudWebUrl } from './cloudWebUrl';

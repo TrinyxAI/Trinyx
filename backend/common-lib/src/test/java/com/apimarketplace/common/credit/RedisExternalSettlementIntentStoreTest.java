e0d27c35e15fe3aee2228f576862940e69c49b83
Trinyx <eddine.rabii@gmail.com>
2026-08-25 03:29:05 +0200

    test(billing): assert atomic producer settlement transitions

--- backend/common-lib/src/test/java/com/apimarketplace/common/credit/RedisExternalSettlementIntentStoreTest.java
+++ backend/common-lib/src/test/java/com/apimarketplace/common/credit/RedisExternalSettlementIntentStoreTest.java
@@ -23,4 +23,30 @@ class RedisExternalSettlementIntentStoreTest {
                 .contains("PSETEX', KEYS[1]")
                 .contains("PSETEX', KEYS[2]");
     }
+
+    @Test
+    void intentPersistenceAndFinalizationAreSingleRedisScripts() throws Exception {
+        String persist = script("PERSIST_INTENT");
+        assertThat(persist)
+                .contains("PSETEX', KEYS[1]")
+                .contains("ZADD', KEYS[2]");
+
+        String acknowledge = script("ACKNOWLEDGE_INTENT");
+        assertThat(acknowledge)
+                .contains("operation['state'] = ARGV[2]")
+                .contains("ZREM', KEYS[3]")
+                .contains("DEL', KEYS[1], KEYS[2]");
+
+        String dead = script("DEAD_LETTER_INTENT");
+        assertThat(dead)
+                .contains("operation['state'] = 'SETTLEMENT_FAILED'")
+                .contains("PSETEX', KEYS[1]")
+                .contains("ZREM', KEYS[4]");
+    }
+
+    private static String script(String fieldName) throws Exception {
+        Field field = RedisExternalSettlementIntentStore.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return ((DefaultRedisScript<?>) field.get(null)).getScriptAsString();
+    }
 }

237b0eb5cb3488aa3161b82616d82d2e4831b663
Trinyx <eddine.rabii@gmail.com>
2026-09-02 23:33:26 +0200

    chore(platform): repin privileged workflows to W4

--- platform/automation/static_policy.py
+++ platform/automation/static_policy.py
@@ -11,7 +11,7 @@ ROOT = Path(__file__).resolve().parents[2]
 WORKFLOWS = ROOT / ".github" / "workflows"
 PINNED_BUILDER_WORKFLOW_COMMIT = "114a2613e8090f034925a1bcf148f055653c3a06"
 PINNED_CONTROL_PLANE_CODE_COMMIT = "e160e3e1c12995ad522a936c95061e03c174f8d8"
-PINNED_PRIVILEGED_WORKFLOW_COMMIT = "f5fc438d9d0ce785e5a6716e41be199bc97591e0"
+PINNED_PRIVILEGED_WORKFLOW_COMMIT = "b5000a7167f12a555ca351c78af1079cabe4486c"
 ANY_USE = re.compile(r"^\s*uses:\s*([^\s@]+)@([^\s#]+)", re.M)
 APP_BUILDS = {
     "build-release-candidate.yml", "build-trinyx-backend.yml",
--- platform/tests/test-aws-oidc-bootstrap.sh
+++ platform/tests/test-aws-oidc-bootstrap.sh
@@ -6,7 +6,7 @@ python3 - "$ROOT/platform/aws/bootstrap/github-oidc-staging-bootstrap.json" <<'P
 import json,re,sys
 doc=json.load(open(sys.argv[1],encoding='utf-8'))
 resources=doc['Resources']
-assert doc['Parameters']['PlatformWorkflowRef']['Default']=='f5fc438d9d0ce785e5a6716e41be199bc97591e0'
+assert doc['Parameters']['PlatformWorkflowRef']['Default']=='b5000a7167f12a555ca351c78af1079cabe4486c'
 assert doc['Parameters']['PlatformWorkflowRef']['AllowedPattern']=='^[0-9a-f]{40}$'
 provider=resources['GitHubActionsOidcProvider']
 assert provider['Type']=='AWS::IAM::OIDCProvider'
